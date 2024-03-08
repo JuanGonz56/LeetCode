aa0918af9b0acdf1ae085303b44236f2abef8911
Juan Gonzalez <gonzalez.juanant524@csu.fullerton.edu>
2024-03-08 14:16:53 -0800

    Fixed Soulution and new problem added

--- MergingStrings/mergeAltStr.py
+++ MergingStrings/mergeAltStr.py
@@ -1,21 +1,22 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+
+        mergedStr = []
         i = 0
-        j = 0
-        mergStr = ''
-        while i < len(word1) and j < len(word2):
-            mergStr += word1[i]
-            mergStr += word2[j]
-            i += 1
-            j += 1
-        
-        while i < len(word1):
-            mergStr += word1[i]
+
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                mergedStr.append(word1[i])
+            
+            if i < len(word2):
+                mergedStr.append(word2[i])
+
             i += 1
 
-        while j < len(word2):
-            mergStr += word2[j]
-            j += 1
-            
-        return mergStr
-    
+        return ''.join(mergedStr)
+        
