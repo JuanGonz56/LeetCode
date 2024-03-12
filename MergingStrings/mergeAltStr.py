7cf36b437b70f54623433a260cdb6cc11290118c
Juan Gonzalez <gonzalez.juanant524@csu.fullerton.edu>
2024-03-11 22:11:51 -0700

    Updated answer for alternating strings

--- MergingStrings/mergeAltStr.py
+++ MergingStrings/mergeAltStr.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
 
-        mergedStr = []
-        i = 0
-
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                mergedStr.append(word1[i])
-            
-            if i < len(word2):
-                mergedStr.append(word2[i])
-
+        result = []
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
             i += 1
+            j += 1
 
-        return ''.join(mergedStr)
-        
+        result.append(word1[i:])
+        result.append(word2[j:])
+
+        return "".join(result)
