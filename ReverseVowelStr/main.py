b6be08f1c45211a7c9b63d6bce7e996da8c5ecd2
Juan Gonzalez <gonzalez.juanant524@csu.fullerton.edu>
2024-03-14 22:38:50 -0700

    Reverse Vowel String problem Solved.

--- ReverseVowelStr/main.py
+++ ReverseVowelStr/main.py
@@ -1,5 +1,19 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        vowels = set('aeiouAEIOU')
         strVowel = []
+        placeVal = []
 
-       
+        for i in range(len(s)):
+            if s[i] in vowels:  # Check if the character is a vowel
+                strVowel.append(s[i])
+                placeVal.append(i)  # Append the index of the vowel
+        
+        strVowel.reverse()
+
+        # Construct the result string by replacing vowels with reversed vowels
+        result = list(s)
+        for j in range(len(placeVal)):
+            result[placeVal[j]] = strVowel[j]
+
+        return ''.join(result)
