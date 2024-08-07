4ad9e1d981598de98a1aaab00f514bf45d6e9a34
Juan Gonzalez <gonzalez.juanant524@csu.fullerton.edu>
2024-08-06 23:08:50 -0700

    fixed solution
    increasing triplet solution fixed

--- 334. Increasing Triplet Subsequence/main.py
+++ 334. Increasing Triplet Subsequence/main.py
@@ -1,7 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-
-        for i in range(len(nums) -2):
-            if nums[i] < nums[i + 1] & nums[i + 1] < nums[i + 2]:
+        first = second = float('inf')
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
                 return True
-        return False 
+        return False
